561225336fb1d7e0f8dd34f5473a50364ee33f2e
Anish59312 <anish31442@gmail.com>
2024-02-15 14:14:19 +0530

    nothing

--- Lab_8/firstapp/urls.py
+++ Lab_8/firstapp/urls.py
@@ -1,9 +1,10 @@
 # blog/urls.py
 from django.urls import path
-from .views import home
+from .views import home, blog
 
 
 urlpatterns = [
     path('home/', home, name='home'),
+    path('blog/', blog, name='blog'),
     # Add other URL patterns as needed
 ]
--- Lab_8/firstapp/views.py
+++ Lab_8/firstapp/views.py
@@ -1,9 +1,26 @@
 # blog/views.py
 from django.shortcuts import render, redirect
 from .models import Blog,BlogPost
+from .forms import BlogForm
+from django.contrib.auth.models import User
 
 def home(request):
     blogs = Blog.objects.all()
     blog_posts = BlogPost.objects.all()
     return render(request, 'home.html', {'blogs': blogs, 'blog_posts': blog_posts})
 
+def blog(request):
+    if request.method == "POST":
+        print("method post for blog")
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            Blog.objects.create(
+                blog_title = form.cleaned_data['title'],
+                user = User.objects.get(username='anish')
+            )
+        return render(request, 'home.html')
+    else:
+        print("non post method call for blog")
+        form = BlogForm()
+        return render(request, 'blog.html', {'form' : form} )
+    
